4d07b6bf17a07c74ef7888b621f509ca9586da42
JAEUK KIM <wodnr76@naver.com>
2023-10-24 11:12:15 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+
+        int result = (int)Math.pow(Math.pow(2, n) + 1, 2);
+
+        System.out.println(result);
+
+
+
+    }
+}
