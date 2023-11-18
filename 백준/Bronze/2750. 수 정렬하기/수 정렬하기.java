cded04c2ffa2feb091eae8086570fd880c75e294
JAEUK KIM <wodnr76@naver.com>
2023-11-18 21:25:37 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 
 public class Main {
@@ -14,32 +15,17 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int tmp = -1;
+
         StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i=0; i<arr.length -1 ; i++){
-            for(int j=i+1; j<arr.length; j++){
-
-                if(arr[i] > arr[j]){
-
-                    tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-
-                }
-
-            }
-
-        }
-
+        Arrays.sort(arr);
+        
         for(int val : arr){
-
             sb.append(val).append("\n");
-
         }
 
         System.out.println(sb);
