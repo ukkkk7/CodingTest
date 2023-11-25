2cc0255e8045d81b922a1d5fb335cfb11bd2f8fd
JAEUK KIM <wodnr76@naver.com>
2023-11-25 21:57:44 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 
@@ -39,29 +40,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int num = Integer.parseInt(br.readLine());
+        char[] num = br.readLine().toCharArray();
 
-        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(num);
 
-        while (num > 0){
-            list.add(num % 10);
-            num /= 10;
+        for(int i=num.length-1; i>=0; i--){
+            System.out.print(num[i]);
         }
-
-        Collections.sort(list, Collections.reverseOrder());
-
-       for(int val : list){
-
-          sb.append(val);
-
-       }
-
-
-        System.out.println(sb);
-
-
 
 
 
