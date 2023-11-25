a6e17b016b00d9800d9d934b69bf9a386c26dee9
JAEUK KIM <wodnr76@naver.com>
2023-11-25 21:42:10 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/*
+
+문제
+배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+
+입력
+첫째 줄에 정렬하려고 하는 수 N이 주어진다. N은 1,000,000,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 자리수를 내림차순으로 정렬한 수를 출력한다.
+
+예제 입력 1
+2143
+예제 출력 1
+4321
+예제 입력 2
+999998999
+예제 출력 2
+999999998
+예제 입력 3
+61423
+예제 출력 3
+64321
+
+
+
+
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (num > 0){
+            list.add(num % 10);
+            num /= 10;
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+       for(int val : list){
+
+          sb.append(val);
+
+       }
+
+
+        System.out.println(sb);
+
+
+
+
+
+    }
+}
