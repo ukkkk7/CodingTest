6b540b77895aa6aa9b346e391f87f928901611e6
JAEUK KIM <wodnr76@naver.com>
2023-12-18 08:25:38 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45512 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,42 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+
+이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+
+예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
+
+N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> q = new LinkedList<>();
+
+        for(int i=1; i<=n; i++){
+            q.offer(i);
+        }
+
+        while (q.size() > 1 ){
+            q.poll();
+            q.offer(q.poll());
+        }
+
+
+        System.out.println(q.poll());
+
+
+    }
+
+}
