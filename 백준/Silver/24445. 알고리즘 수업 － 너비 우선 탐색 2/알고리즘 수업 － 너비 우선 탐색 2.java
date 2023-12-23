a78c797b7b9b1718b94165005d661dfa4ee1a585
JAEUK KIM <wodnr76@naver.com>
2023-12-23 21:26:45 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1168 ms, Memory: 87908 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -1,75 +1,99 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+
+import java.io.*;
 import java.util.*;
+
 
 public class Main {
 
-    /**
-     *
-     * 오늘도 서준이는 너비우선탐색(BFS) 수업조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자
-     * N개의 정점과 M개의 간선으로 구성된 무방향 그래프가 주어진다. 정정 번호는 1번부터 N번이고 모든 간선의 가중치는 1이다.
-       정점 R에서 시작하여 너비 우선 탐새으로 노드를 방문할 경우 노드의 방문 순서를 출려ㄱ하자
-     * 노드의 방문 순서를 출력하자
-     * */
-
-
-    static int N,M,R;
-    static boolean visited[];
-    static ArrayList<ArrayList<Integer>> graph =new ArrayList<>();
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    static boolean[] visit;
+    static int n,m,r,cnt;
+    static List<ArrayList<Integer>> graph = new ArrayList<>();
+    static int[] result;
+    public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());   //정점의 개수
-        M = Integer.parseInt(st.nextToken()); //간선의 개수
-        R = Integer.parseInt(st.nextToken());  //시작 점
+        // 정점, 간선 수, 시작노드 입력받기
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N+1];
-        for (int i = 0; i <= N; i++) {
+
+        //정점의 수 만큼 visit배열 초기화
+        visit = new boolean[n+1]; // 노드가 1번부터이기 때문에 1더함
+
+        //graph 초기화
+        for(int i=0; i<n+1; i++){
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < M; i++) {
+        for(int i=0; i<m; i++){
+
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            //간선 정보 입력받기
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-        for(List<Integer> l : graph){
-            Collections.sort(l,Collections.reverseOrder());
+            // 간선정보는 양방향
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
 
-        BFS(R);
+        for(int i=1; i<n+1; i++){
+            //그래프 내림차순 정렬이기 때문에 내림차순으로 변경
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        cnt = 1;
+
+        bfs(r);
+
+        for(int i=1; i< result.length; i++){
+            sb.append(result[i]).append("\n");
+        }
+
+        System.out.println(sb);
 
 
     }
 
-    private static void BFS(int r) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(r);
-        visited[r] = true;
-        int cnt = 0;
-        int count[] = new int[N+1];
-        while (!queue.isEmpty()){
-            int now = queue.poll();
-            count[now] = ++cnt;
-            for(int i =0;i<graph.get(now).size();i++){
 
 
-                int nextVertex = graph.get(now).get(i);
-                if(!visited[nextVertex]){
-                    visited[nextVertex] = true;
-                    queue.add(nextVertex);
+    private static void bfs(int node){
+
+        //시작 노드 방문 체크
+        visit[node]= true;
+        //순서 저장하기 위한 배열 추가
+        result = new int[n+1];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(node);
+
+        result[node] = cnt++;
+
+        while (!q.isEmpty()){
+            int a = q.poll();
+
+            for(int i=0; i<graph.get(a).size(); i++){
+
+                int newNode = graph.get(a).get(i);
+
+                if(!visit[newNode]){
+                    q.add(newNode);
+                    result[newNode] = cnt++;
+                    visit[newNode] = true;
                 }
+
             }
+
         }
-        for(int i=1;i<=N;i++) System.out.println(count[i]);
+
 
 
     }
+
 }
