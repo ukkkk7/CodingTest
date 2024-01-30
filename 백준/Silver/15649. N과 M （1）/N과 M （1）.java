238c1bbf519c55bd274593c6aa9ace28af716451
JAEUK KIM <wodnr76@naver.com>
2024-01-30 09:22:40 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 22724 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n,m;
+    static int[] arr;
+    static boolean[] visit;
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visit = new boolean[n];
+
+        dfs(0);
+
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int depth){
+
+            if(depth == m){
+                for(int val : arr){
+                    sb.append(val).append(" ");
+                }
+                sb.append("\n");
+                return;
+            }
+
+
+        for(int i=0; i<n; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                arr[depth] = i+1;
+                dfs(depth+1);
+                visit[i] = false;
+            }
+        }
+
+
+
+    }
+
+
+}
