1c5766fbbc475c347eae9ba9f9e8330abf18c146
JAEUK KIM <wodnr76@naver.com>
2024-02-01 09:09:15 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 15340 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+
+ static int n,m;
+ static int[] arr;
+ static StringBuilder sb = new StringBuilder();
+ public static void main(String[] args) throws IOException{
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  StringTokenizer st = new StringTokenizer(br.readLine());
+
+  n = Integer.parseInt(st.nextToken());
+  m = Integer.parseInt(st.nextToken());
+
+  arr = new int[m];
+
+  dfs(1,0);
+  System.out.println(sb);
+
+ }
+
+ public static void dfs(int at, int depth) {
+
+  if(depth == m){
+   for(int val : arr){
+    sb.append(val).append(" ");
+   }
+   sb.append("\n");
+   return;
+  }
+
+  for(int i=at; i<=n; i++){
+   arr[depth] = i;
+   dfs(i, depth+1);
+  }
+
+ }
+
+}
